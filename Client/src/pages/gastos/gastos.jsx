7696130b3efbaa594103850aa46a2000ae956816
Pablo Risi <pablomrisi@hotmail.com>
2024-12-13 12:14:44 -0300

    carpetas

--- Client/src/pages/gastos/gastos.jsx
+++ Client/src/pages/gastos/gastos.jsx
@@ -12,8 +12,8 @@ import digital from "../../assets/digital.mp3"
 import ok from "../../assets/ok.mp3"
 
 
-const serverFront = 'http://localhost:3001'
-// const serverFront = 'https://ventas-de-negocio.onrender.com'
+// const serverFront = 'http://localhost:3001'
+const serverFront = 'https://ventas-de-negocio.onrender.com'
 
 
 export function Gastos(){
