66aee21a316f4777d521a45b74cdb5e16829fcb5
Pablo Risi <pablomrisi@hotmail.com>
2024-12-17 12:58:50 -0300

    cambios de error Model

--- Server/index.js
+++ Server/index.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const GastosModel = require('./models/gastos');
 const VentasModel = require('./Model/ventas');
-// const ProductoModel = require('./models/productos');
+const ProductoModel = require('./Model/productos');
 const ProveedorModel = require('./Model/proveedor');
-// const StockModel = require('./models/stock');
-// const File = require ('./models/files')
-// const multer = require('multer');
-// const NotasModel = require('./models/notas');
+const StockModel = require('./Model/stock');
+const File = require ('./Model/files')
+const multer = require('multer');
+const NotasModel = require('./Model/notas');
 
 
 require("dotenv").config();
@@ -152,143 +152,143 @@ app.patch('/edit-gastos/:id', async (req, res) => {
 
 
 
-// // Productos
+// Productos
 
-// // Obtener datos de productos
-// app.get('/productos', async (req, res) => {
-//     try {
-//         const productos = await ProductoModel.find();
-//         res.json(productos);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// });
+// Obtener datos de productos
+app.get('/productos', async (req, res) => {
+    try {
+        const productos = await ProductoModel.find();
+        res.json(productos);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
-// // Agregar productos
-// app.post('/add-productos', async (req, res) => {
-//     const { marca, mascota, edad,condicion, kilo, precio, categoria } = req.body;
-//     if (!marca || !mascota || !edad || !condicion || !kilo || !precio || !categoria) {
-//         return res.status(400).json({ error: 'Faltan datos requeridos' });
-//     }
-//     try {
-//         const newProducto = new ProductoModel({ marca, mascota, edad, condicion, kilo, precio, categoria });
-//         const result = await newProducto.save();
-//         res.json(result);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: err.message });
-//     }
-// });
+// Agregar productos
+app.post('/add-productos', async (req, res) => {
+    const { marca, mascota, edad,condicion, kilo, precio, categoria } = req.body;
+    if (!marca || !mascota || !edad || !condicion || !kilo || !precio || !categoria) {
+        return res.status(400).json({ error: 'Faltan datos requeridos' });
+    }
+    try {
+        const newProducto = new ProductoModel({ marca, mascota, edad, condicion, kilo, precio, categoria });
+        const result = await newProducto.save();
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+});
 
-// // Eliminar productos
-// app.delete('/delete-productos/:id', async (req, res) => {
-//     const { id } = req.params;
-//     try {
-//         const result = await ProductoModel.findByIdAndDelete(id);
-//         res.json(result);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// });
+// Eliminar productos
+app.delete('/delete-productos/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await ProductoModel.findByIdAndDelete(id);
+        res.json(result);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
-// // Eliminar varios productos
-// app.delete('/delete-many-productos/', async (req,res) => {
-//     const {ids} = req.body;
+// Eliminar varios productos
+app.delete('/delete-many-productos/', async (req,res) => {
+    const {ids} = req.body;
 
-//     if (!Array.isArray(ids) || ids.length  === 0)
-//         return res.status(400).json({error:'se require un array en los Ids'})
+    if (!Array.isArray(ids) || ids.length  === 0)
+        return res.status(400).json({error:'se require un array en los Ids'})
 
-//     try {
-//         const result = await ProductoModel.deleteMany({_id: {$in:ids}})
-//         res.json( { message:`${result.deletedCount} productos eliminados`, result })
-//     } catch (err) {
-//         res.status(500).json({ error: "Server error: " + err.message });
-//     }
-// })
+    try {
+        const result = await ProductoModel.deleteMany({_id: {$in:ids}})
+        res.json( { message:`${result.deletedCount} productos eliminados`, result })
+    } catch (err) {
+        res.status(500).json({ error: "Server error: " + err.message });
+    }
+})
 
-// // Editar productos
-// app.patch('/edit-productos/:id', async (req, res) => {
-//     const { id } = req.params;
-//     const { marca, mascota, edad, condicion, kilo, precio } = req.body;
-//     try {
-//         const result = await ProductoModel.findByIdAndUpdate(id, { marca, mascota, edad, condicion, kilo, precio }, { new: true });
-//         res.json(result);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// });
-
-
-
-// // Notas
-
-// // Ruta para obtener notas
-// app.get('/notas', async (req, res) => {
-//     try {
-//         const notas = await NotaModel.find();
-//         res.json(notas);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// });
+// Editar productos
+app.patch('/edit-productos/:id', async (req, res) => {
+    const { id } = req.params;
+    const { marca, mascota, edad, condicion, kilo, precio } = req.body;
+    try {
+        const result = await ProductoModel.findByIdAndUpdate(id, { marca, mascota, edad, condicion, kilo, precio }, { new: true });
+        res.json(result);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 
-// // Ruta para añadir una nota
-// app.post('/add-notas', async (req, res) => {
-//     const { notas, meses, description } = req.body;
-//     if (!notas || !meses || !description) {
-//         return res.status(400).json({ error: 'Nota no proporcionada' });
-//     }
-//     try {
-//         const newNota = new NotaModel({ notas, meses, description});
-//         const result = await newNota.save();
-//         res.json(result);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// });
 
-// // Eliminar notas
-// app.delete('/delete-notas/:id', (req, res) => {
-//     const { id } = req.params;
-//     NotaModel.findByIdAndDelete(id)
-//         .then(result => res.json(result))
-//         .catch(err => res.status(500).json({ error: err.message }));
-// });
+// Notas
 
-// // Eliminar varias notas
-// app.delete('/delete-many-notas/', async (req,res) => {
-//     const {ids} = req.body;
+// Ruta para obtener notas
+app.get('/notas', async (req, res) => {
+    try {
+        const notas = await NotaModel.find();
+        res.json(notas);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+// Ruta para añadir una nota
+app.post('/add-notas', async (req, res) => {
+    const { notas, meses, description } = req.body;
+    if (!notas || !meses || !description) {
+        return res.status(400).json({ error: 'Nota no proporcionada' });
+    }
+    try {
+        const newNota = new NotaModel({ notas, meses, description});
+        const result = await newNota.save();
+        res.json(result);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Eliminar notas
+app.delete('/delete-notas/:id', (req, res) => {
+    const { id } = req.params;
+    NotasModel.findByIdAndDelete(id)
+        .then(result => res.json(result))
+        .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// Eliminar varias notas
+app.delete('/delete-many-notas/', async (req,res) => {
+    const {ids} = req.body;
     
-//     if(!Array.isArray(ids) || ids.length === 0)
-//         return res.status(400).json({error: 'se require un array en los Ids'})
+    if(!Array.isArray(ids) || ids.length === 0)
+        return res.status(400).json({error: 'se require un array en los Ids'})
 
-//     try{
-//         const result = await NotasModel.deleteMany({ _id:{$in:ids}})
-//         res.json({message: `${result.deletedCount} productos eliminados`, result})
-//     } catch (err) {
-//         res.status(500).json({error: "Server error: " + err.message })
-//     }
-// })
+    try{
+        const result = await NotasModel.deleteMany({ _id:{$in:ids}})
+        res.json({message: `${result.deletedCount} productos eliminados`, result})
+    } catch (err) {
+        res.status(500).json({error: "Server error: " + err.message })
+    }
+})
 
-// // Editar notas
-// app.patch('/edit-notas/:id', (req, res) => {
-//     const { id } = req.params;
-//     const { notas, meses, description } = req.body;
-//     NotaModel.findByIdAndUpdate(id, { notas, meses, description }, { new: true })
-//         .then(result => res.json(result))
-//         .catch(err => res.status(500).json({ error: err.message }));
-// });
+// Editar notas
+app.patch('/edit-notas/:id', (req, res) => {
+    const { id } = req.params;
+    const { notas, meses, description } = req.body;
+    NotasModel.findByIdAndUpdate(id, { notas, meses, description }, { new: true })
+        .then(result => res.json(result))
+        .catch(err => res.status(500).json({ error: err.message }));
+});
 
 
-// // Completar notas
-// app.patch('/completed-notas/:id', (req, res) => {
-//     const { id } = req.params;
-//     const { completed } = req.body;
-//     NotaModel.findByIdAndUpdate(id, { completed }, { new: true })
-//         .then(result => res.json(result))
-//         .catch(err => res.status(500).json({ error: err.message }));
-// });
+// Completar notas
+app.patch('/completed-notas/:id', (req, res) => {
+    const { id } = req.params;
+    const { completed } = req.body;
+    NotasModel.findByIdAndUpdate(id, { completed }, { new: true })
+        .then(result => res.json(result))
+        .catch(err => res.status(500).json({ error: err.message }));
+});
 
 
 // Proveedores
@@ -361,136 +361,136 @@ app.patch('/edit-proveedors/:id', async (req, res) => {
 
 // Stock
 
-// // Obtener stock
-// app.get('/stock', async (req, res) => {
-//     try {
-//         const stock = await StockModel.find();
-//         res.json(stock);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// });
+// Obtener stock
+app.get('/stock', async (req, res) => {
+    try {
+        const stock = await StockModel.find();
+        res.json(stock);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 
 
-// // Agregar stock
-// app.post('/add-stock', async (req, res) => {
-//     const { brands, pet, size, kg, amount, condition } = req.body;
-//     if (!brands || !pet || !size || !kg || !amount || !condition) {
-//         return res.status(400).json({ error: 'Faltan datos requeridos' });
-//     }
-//     try {
-//         const newStock = new StockModel({ brands, pet, size, kg, amount, condition });
-//         const result = await newStock.save();
-//         res.json(result);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: err.message });
-//     }
-// });
+// Agregar stock
+app.post('/add-stock', async (req, res) => {
+    const { brands, pet, size, kg, amount, condition } = req.body;
+    if (!brands || !pet || !size || !kg || !amount || !condition) {
+        return res.status(400).json({ error: 'Faltan datos requeridos' });
+    }
+    try {
+        const newStock = new StockModel({ brands, pet, size, kg, amount, condition });
+        const result = await newStock.save();
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+});
 
-// //Eliminar Stock
-// app.delete('/delete-stock/:id', async (req, res) => {
-//     const { id } = req.params;
-//     try {
-//         const stock = await StockModel.findByIdAndDelete(id);
-//         if (!stock) {
-//             return res.status(404).json({ error: 'Stock not found' });
-//         }
-//         res.json(stock);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// })
+//Eliminar Stock
+app.delete('/delete-stock/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const stock = await StockModel.findByIdAndDelete(id);
+        if (!stock) {
+            return res.status(404).json({ error: 'Stock not found' });
+        }
+        res.json(stock);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
 
-// app.delete('/delete-many-stocks/', async (req,res) => {
-//     const {ids} = req.body;
+app.delete('/delete-many-stocks/', async (req,res) => {
+    const {ids} = req.body;
 
-//     if (!Array.isArray(ids) || ids.length  === 0)
-//         return res.status(400).json({error:'se require un array en los Ids'})
+    if (!Array.isArray(ids) || ids.length  === 0)
+        return res.status(400).json({error:'se require un array en los Ids'})
 
-//     try {
-//         const result = await StockModel.deleteMany({_id: {$in:ids}})
-//         res.json( { message:`${result.deletedCount} productos eliminados`, result })
-//     } catch (err) {
-//         res.status(500).json({ error: "Server error: " + err.message });
-//     }
-// })
-
-
-// // Editar Stock
-// app.patch('/edit-stock/:id', async ( req, res ) => {
-//     const {id} = req.params;
-//     const { brands, pet, size, kg, amount, condition } = req.body;
-//     try{
-//         const result = await StockModel.findByIdAndUpdate(id, { brands, pet, size, kg, amount, condition }, {new: true} );
-//         res.json(result)
-//     } catch (err) {
-//         res.status(500).json({ error:err.message })
-//     }
-// })
-
-// // Configuración de multer
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage });
-
-// // Ruta para subir archivos
-// app.post('/upload', upload.single('file'), async (req, res) => {
-//     const file = new File({
-//         filename: req.file.originalname,
-//         contentType: req.file.mimetype,
-//         data: req.file.buffer
-//     });
-
-//     try {
-//         await file.save();
-//         res.status(201).send({ message: 'Archivo subido exitosamente' });
-//     } catch (error) {
-//         res.status(500).send({ message: 'Error al subir el archivo', error });
-//     }
-// });
-
-// // Ruta para listar archivos
-// app.get('/files', async (req, res) => {
-//     try {
-//         const files = await File.find({});
-//         res.status(200).json(files);
-//     } catch (error) {
-//         res.status(500).send({ message: 'Error al listar archivos', error });
-//     }
-// });
+    try {
+        const result = await StockModel.deleteMany({_id: {$in:ids}})
+        res.json( { message:`${result.deletedCount} productos eliminados`, result })
+    } catch (err) {
+        res.status(500).json({ error: "Server error: " + err.message });
+    }
+})
 
 
-// // Ruta para descargar/ver un archivo
-// app.get('/files/:id', async (req, res) => {
-//     try {
-//         const file = await File.findById(req.params.id);
+// Editar Stock
+app.patch('/edit-stock/:id', async ( req, res ) => {
+    const {id} = req.params;
+    const { brands, pet, size, kg, amount, condition } = req.body;
+    try{
+        const result = await StockModel.findByIdAndUpdate(id, { brands, pet, size, kg, amount, condition }, {new: true} );
+        res.json(result)
+    } catch (err) {
+        res.status(500).json({ error:err.message })
+    }
+})
 
-//         // Verificar el tipo de contenido (en este caso, application/pdf para PDFs)
-//         res.set({
-//             'Content-Type': file.contentType, // Esto debería ser 'application/pdf' para PDFs
-//             'Content-Disposition': `inline; filename=${file.filename}` // inline para visualización
-//         });
+// Configuración de multer
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-//         // Enviar el archivo al cliente
-//         res.send(file.data);
-//     } catch (error) {
-//         res.status(500).send({ message: 'Error al obtener el archivo', error });
-//     }
-// });
+// Ruta para subir archivos
+app.post('/upload', upload.single('file'), async (req, res) => {
+    const file = new File({
+        filename: req.file.originalname,
+        contentType: req.file.mimetype,
+        data: req.file.buffer
+    });
+
+    try {
+        await file.save();
+        res.status(201).send({ message: 'Archivo subido exitosamente' });
+    } catch (error) {
+        res.status(500).send({ message: 'Error al subir el archivo', error });
+    }
+});
+
+// Ruta para listar archivos
+app.get('/files', async (req, res) => {
+    try {
+        const files = await File.find({});
+        res.status(200).json(files);
+    } catch (error) {
+        res.status(500).send({ message: 'Error al listar archivos', error });
+    }
+});
 
 
-// app.delete('/delete-files/:id', async (req,res) => {
-//     const { id } = req.params;
-//     try {
-//         const deleteFiles = await File.findByIdAndDelete(id)
-//         if (!deleteFiles){
-//             return res.status(404).json({ error: 'files not found'})
-//         }
-//         res.json(deleteFiles)
-//     } catch {
-//         res.status(500).json( {error:err.message})
-//     }
-// })
+// Ruta para descargar/ver un archivo
+app.get('/files/:id', async (req, res) => {
+    try {
+        const file = await File.findById(req.params.id);
+
+        // Verificar el tipo de contenido (en este caso, application/pdf para PDFs)
+        res.set({
+            'Content-Type': file.contentType, // Esto debería ser 'application/pdf' para PDFs
+            'Content-Disposition': `inline; filename=${file.filename}` // inline para visualización
+        });
+
+        // Enviar el archivo al cliente
+        res.send(file.data);
+    } catch (error) {
+        res.status(500).send({ message: 'Error al obtener el archivo', error });
+    }
+});
+
+
+app.delete('/delete-files/:id', async (req,res) => {
+    const { id } = req.params;
+    try {
+        const deleteFiles = await File.findByIdAndDelete(id)
+        if (!deleteFiles){
+            return res.status(404).json({ error: 'files not found'})
+        }
+        res.json(deleteFiles)
+    } catch {
+        res.status(500).json( {error:err.message})
+    }
+})
 
 
 app.listen(3001, () => {
