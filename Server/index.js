a21933931bfc8f38331d9932a0010c48c038e6ca
Pablo Risi <pablomrisi@hotmail.com>
2025-11-10 11:50:22 -0300

    cors options

--- Server/index.js
+++ Server/index.js
@@ -22,11 +22,12 @@ app.use(express.json());
 const corsOptions = {
     origin: ['http://localhost:5173','http://localhost:5174', 'http://localhost:5175', 'https://ventas-de-negocio.onrender.com', 'https://ventas-de-negocio.vercel.app'],
     credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 mongoose
     .connect(process.env.MONGODB)
